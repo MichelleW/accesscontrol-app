2c93ce6b6510a323c03669b9f454bf0949bb2926
Michelle <michelle.wu177@gmail.com>
2025-07-15 18:43:18 -0700

    <RoleCard/> first draft

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import './components/VerkadaPlayer/style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import RoleCard from './components/RoleCard/RoleCard';
 
+const users = [{
+  name: "Jasmine Lee",
+  role: "Employee",
+  avatar: "https://i.pravatar.cc/150?img=37"
+},{
+  name: "John Lee",
+  role: "Admin",
+  avatar: "https://i.pravatar.cc/150?img=35"
+},{
+  name: "Michelle Lee",
+  role: "Visitor",
+  avatar: "https://i.pravatar.cc/150?img=32"
+}]
 
 
 function App() {
@@ -13,7 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      <RoleCard/>
+      <RoleCard users={users}/>
     </div>
   );
 }
--- src/components/RoleCard/RoleCard.js
+++ src/components/RoleCard/RoleCard.js
@@ -1,27 +1,38 @@
-import React from 'react'
 import './RoleCard.css'
-function RoleCard() {
+function RoleCard({ users }) {
+
+    function getRoleColor(role) {
+        if (!role) return ''
+        if (role.toLowerCase() === 'admin') return 'blue'
+        if (role.toLowerCase() === 'employee') return 'green'
+        if (role.toLowerCase() === 'visitor') return 'gray'
+        return ''
+    }
+
     return (
         <div className='role-card'>
-            <div className='card'>
-                <div className='profile'>
-                    <div className='profile-pic'>
-                        <img src='./profilepix.jpg' alt='profile' />
-                    </div>
-                    <div className='user-info'>
-                        <div className='name'>
-                            <h3>John Doe</h3>
+            {
+                users.map((user, index) => (
+                    <div key={user.id || index} className={`card ${getRoleColor(user.role)}`}>
+                        <div className='profile'>
+                            <div className='profile-pic'>
+                                <img src={user.avatar} alt='profile' />
+                            </div>
+                            <div className='user-info'>
+                                <div className='name'>
+                                    <h3>{user.name}</h3>
+                                </div>
+                                <div className='role'>
+                                    <p>{user.role}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className='role'>
-                            <p>Admin</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="profile-dec">Lorem, ipsum dolor sit amet consectetur adipisicing elit. </div>
-                <div className="btn btn-outline-secondary">
-                    Edit Role
-                </div>
-            </div>
+                        <div className="profile-dec">Lorem, ipsum dolor sit amet consectetur adipisicing elit. </div>
+                        <button className="btn btn-outline-info">
+                            Edit Role
+                        </button>
+                    </div>))
+            }
         </div>
     )
 }
